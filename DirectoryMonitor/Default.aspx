090a51aed2ae992ec6ec16484d06177fe2d78d23
David Holohlavsky <david.holohlavsky@gmail.com>
2020-09-14 00:08:03 +0200

    created file watcher without subdirectories

---
+++ DirectoryMonitor/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DirectoryMonitor.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+  <title></title>
+</head>
+<body>
+  <form id="form1" runat="server">
+    <p>
+      PATH TO LOCAL DIRECTORY
+    </p>
+    <p>
+      <asp:TextBox ID="TextBox1" runat="server" Width="500px"></asp:TextBox>
+      <asp:Button ID="Button1" runat="server" Text="Start" Width="100px" />
+    </p>
+    <asp:Label ID="Label1" runat="server" Text="žádný adresář" Height="200px" Width="600px"></asp:Label>
+  </form>
+</body>
+</html>
---
+++ DirectoryMonitor/Default.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Security.Permissions;
+using System.Text;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace DirectoryMonitor
+{
+  public partial class Default : System.Web.UI.Page
+  {
+
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      try
+      {
+        if (IsPostBack)
+        {
+          string[] fileEntries = Directory.GetFiles(TextBox1.Text);
+
+          List<string> newFilesNamesList = new List<string>();
+          List<string> newFilesLastWriteTimeList = new List<string>();
+          List<int> newVersionOfFileList = new List<int>();
+
+          foreach (string filePath in fileEntries)
+          {
+            FileInfo fileInfo = new FileInfo(filePath);
+            string fileName = filePath.Replace(TextBox1.Text, "");
+            newFilesNamesList.Add($"{fileName}");
+            newFilesLastWriteTimeList.Add($"{fileInfo.LastWriteTime}");
+            newVersionOfFileList.Add(1);
+          }
+          if (ViewState["filesNames"] == null)
+          {
+            Label1.Text = "nový adresář, žádné změny";
+          }
+          else
+          {
+            string oldFilesNames = ViewState["filesNames"].ToString();
+            string oldFilesLastWriteTime = ViewState["filesLastWriteTime"].ToString();
+            string oldFileVersion = ViewState["versionOfFiles"].ToString();
+            List<string> oldFilesNamesList = oldFilesNames.Split(';').ToList();
+            List<string> oldFilesLastWriteTimeList = oldFilesLastWriteTime.Split(';').ToList();
+            List<int> oldFileVersionList = oldFileVersion.Split(';').ToList().ConvertAll(int.Parse);
+            StringBuilder sb = new StringBuilder();
+            foreach (string oldFile in oldFilesNamesList)
+            {
+              if (newFilesNamesList.Contains(oldFile))
+              {
+                int indexNameNew = newFilesNamesList.IndexOf(oldFile);
+                int indexNameOld = oldFilesNamesList.IndexOf(oldFile);
+                // no changes
+                if (newFilesLastWriteTimeList[indexNameNew].Equals(oldFilesLastWriteTimeList[indexNameOld]))
+                {
+                  Label1.Text = "žádná změna";
+                  newVersionOfFileList = oldFileVersionList;
+                }
+                else
+                {
+                  int newVersionOfFile = oldFileVersionList[indexNameOld] + 1;
+                  newVersionOfFileList[indexNameNew] = newVersionOfFile;
+                  sb.Append($"[M] {newFilesNamesList[indexNameNew]} (ve verzi {newVersionOfFile})<br />");
+                }
+              }
+              else
+              {
+                sb.Append($"[D] {oldFile}<br />");
+              }
+            }
+            foreach (string newFile in newFilesNamesList)
+            {
+              if (!oldFilesNamesList.Contains(newFile))
+              {
+                sb.Append($"[A] {newFile}<br />");
+              }
+            }
+            if (!String.IsNullOrEmpty(sb.ToString()))
+            {
+              sb.Append("<br />");
+              sb.Append("[A] = added (nový soubor)<br />");
+              sb.Append("[M] = modified (změněný soubor)<br />");
+              sb.Append("[D] = deleted (odstraněný soubor)<br />");
+              Label1.Text = sb.ToString();
+            }
+          }
+          string filesNames = String.Join(";", newFilesNamesList);
+          string filesLastWriteTime = String.Join(";", newFilesLastWriteTimeList);
+          string versionOfFile = String.Join(";", newVersionOfFileList);
+          ViewState["filesNames"] = filesNames;
+          ViewState["filesLastWriteTime"] = filesLastWriteTime;
+          ViewState["versionOfFiles"] = versionOfFile;
+        }
+      }
+      catch (Exception ex)
+      {
+        log.Error("This is my error", ex);
+      }
+    }
+  }
+}
