89daa72b40b009e4422684947cd6ab9c0342d8a6
David Holohlavsky <david.holohlavsky@gmail.com>
2020-10-25 02:50:13 +0200

    creating subdirectories scans

--- DirectoryMonitor/Default.aspx.cs
+++ DirectoryMonitor/Default.aspx.cs
@@ -22,11 +22,15 @@ namespace DirectoryMonitor
       {
         if (IsPostBack)
         {
-          string[] fileEntries = Directory.GetFiles(TextBox1.Text);
+          List<string> fileEntries = new List<string>();
+          List<string> subdirectoryEntries = new List<string>();
+          ProcessDirectory(TextBox1.Text, fileEntries, subdirectoryEntries);
+          //string[] fileEntries = Directory.GetFiles(TextBox1.Text);
 
           List<string> newFilesNamesList = new List<string>();
           List<string> newFilesLastWriteTimeList = new List<string>();
           List<int> newVersionOfFileList = new List<int>();
+          List<string> newSubDirectoryNamesList = new List<string>();
 
           foreach (string filePath in fileEntries)
           {
@@ -35,6 +39,11 @@ namespace DirectoryMonitor
             newFilesNamesList.Add($"{fileName}");
             newFilesLastWriteTimeList.Add($"{fileInfo.LastWriteTime}");
             newVersionOfFileList.Add(1);
+          }
+          foreach (string subDirectoryPath in subdirectoryEntries)
+          {
+            string subDirectoryName = subDirectoryPath.Replace(TextBox1.Text, "");
+            newSubDirectoryNamesList.Add($"{subDirectoryName}");
           }
           if (ViewState["filesNames"] == null)
           {
@@ -45,9 +54,11 @@ namespace DirectoryMonitor
             string oldFilesNames = ViewState["filesNames"].ToString();
             string oldFilesLastWriteTime = ViewState["filesLastWriteTime"].ToString();
             string oldFileVersion = ViewState["versionOfFiles"].ToString();
+            string oldSubDirectoryNames = ViewState["subDirectoryNames"].ToString();
             List<string> oldFilesNamesList = oldFilesNames.Split(';').ToList();
             List<string> oldFilesLastWriteTimeList = oldFilesLastWriteTime.Split(';').ToList();
             List<int> oldFileVersionList = oldFileVersion.Split(';').ToList().ConvertAll(int.Parse);
+            List<string> oldSubDirectoryNamesList = oldSubDirectoryNames.Split(';').ToList();
             StringBuilder sb = new StringBuilder();
             foreach (string oldFile in oldFilesNamesList)
             {
@@ -80,6 +91,20 @@ namespace DirectoryMonitor
                 sb.Append($"[A] {newFile}<br />");
               }
             }
+            foreach (string oldSubDirectory in oldSubDirectoryNamesList)
+            {
+              if (!newSubDirectoryNamesList.Contains(oldSubDirectory))
+              {
+                sb.Append($"[D] {oldSubDirectory}(directory)<br />");
+              }
+            }
+            foreach (string newDirectory in newSubDirectoryNamesList)
+            {
+              if (!oldSubDirectoryNamesList.Contains(newDirectory))
+              {
+                sb.Append($"[A] {newDirectory}(directory)<br />");
+              }
+            }
             if (!String.IsNullOrEmpty(sb.ToString()))
             {
               sb.Append("<br />");
@@ -92,14 +117,26 @@ namespace DirectoryMonitor
           string filesNames = String.Join(";", newFilesNamesList);
           string filesLastWriteTime = String.Join(";", newFilesLastWriteTimeList);
           string versionOfFile = String.Join(";", newVersionOfFileList);
+          string subDirectoryNames = String.Join(";", newSubDirectoryNamesList);
           ViewState["filesNames"] = filesNames;
           ViewState["filesLastWriteTime"] = filesLastWriteTime;
           ViewState["versionOfFiles"] = versionOfFile;
+          ViewState["subDirectoryNames"] = subDirectoryNames;
         }
       }
       catch (Exception ex)
       {
         log.Error("This is my error", ex);
+      }
+    }
+    public static void ProcessDirectory(string targetDirectory, List<string> fileEntries, List<string> subdirectoryEntries)
+    {
+      //system.stackoverflowexception
+      fileEntries.AddRange(Directory.GetFiles(targetDirectory).ToList());
+      subdirectoryEntries.AddRange(Directory.GetDirectories(targetDirectory).ToList());
+      foreach(string subdirectory in subdirectoryEntries)
+      {
+        ProcessDirectory(subdirectory, fileEntries, subdirectoryEntries);
       }
     }
   }
